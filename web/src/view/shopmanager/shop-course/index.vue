25088d4425c7fcf4baff7001bc3b1059b2c0f70c
zhoubowen <zhoubowen@mudu.tv>
2020-07-26 20:16:03 +0800

    7/26 update

--- web/src/view/shopmanager/shop-course/index.vue
+++ web/src/view/shopmanager/shop-course/index.vue
@@ -87,18 +87,18 @@
                     </el-table-column>
                 </el-table>
                 <el-dialog  :visible.sync="dialogVisible" title="添加课程" width="30%">
-                    <el-form ref="shopform" :model="formData" :rules="rules" size="medium" label-width="100px"
+                    <el-form ref="courseform" :model="formData" :rules="rules" size="medium" label-width="100px"
                              label-position="left">
                         <el-form-item label="课程名称" prop="coursename">
                             <el-input v-model="formData.coursename" placeholder="请输入课程名称" clearable :style="{width: '100%'}">
                             </el-input>
                         </el-form-item>
                         <el-form-item label="线上价格" prop="onlineprice">
-                            <el-input v-model="formData.onlineprice" type="string" placeholder="请输入线上价格" clearable :style="{width: '100%'}">
+                            <el-input v-model="formData.onlineprice" type="number" placeholder="请输入线上价格" clearable :style="{width: '100%'}">
                             </el-input>
                         </el-form-item>
                         <el-form-item label="线下价格" prop="offlineprice">
-                            <el-input v-model="formData.offlineprice" placeholder="请输入线下价格" type="string" clearable :style="{width: '100%'}">
+                            <el-input v-model="formData.offlineprice" placeholder="请输入线下价格" type="number" clearable :style="{width: '100%'}">
                             </el-input>
                         </el-form-item>
                         <el-form-item label="时间" prop="starttime">
@@ -111,27 +111,97 @@
                                     end-placeholder="结束日期">
                             </el-date-picker>
                         </el-form-item>
+                        <el-form-item
+                                v-for="(item,index) in desc"
+                                :label="'标题' + (index+1)"
+                                :prop="'desc.' + index + '.value'">
+                            <el-input
+                                    type="text"
+                                    placeholder="请输入标题"
+                                    v-model="desc[index].title"
+                                    maxlength="10"
+                                    show-word-limit
+                            >
+                            </el-input>
+                            <div style="margin: 20px 0;"></div>
+                            <el-input
+                                    type="textarea"
+                                    placeholder="请输入内容"
+                                    v-model="desc[index].content"
+                                    maxlength="30"
+                                    show-word-limit
+                            >
+                            </el-input>
+                            <el-button @click.prevent="addDesc">添加</el-button>
+                            <el-button v-if="index!=0" @click.prevent="removeDesc">删除</el-button>
+                        </el-form-item>
                     </el-form>
                     <div slot="footer">
                         <el-button @click="close">取消</el-button>
                         <el-button type="primary" @click="handelConfirm">确定</el-button>
                     </div>
                 </el-dialog>
-                <el-dialog  :visible.sync="dialogVisible1" title="编辑店铺" width="40%">
-                    <el-form ref="shopform" :model="formData1" :rules="rules1" size="medium" label-width="100px"
+                <el-dialog  :visible.sync="dialogVisible1" title="添加课程" width="30%">
+                    <el-form ref="courseform" :model="formData1" :rules="rules" size="medium" label-width="100px"
                              label-position="left">
+                        <el-form-item label="课程名称" prop="coursename">
+                            <el-input v-model="formData1.coursename" placeholder="请输入课程名称" clearable :style="{width: '100%'}">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="线上价格" prop="onlineprice">
+                            <el-input v-model="formData1.onlineprice" type="number" placeholder="请输入线上价格" clearable :style="{width: '100%'}">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="线下价格" prop="offlineprice">
+                            <el-input v-model="formData1.offlineprice" placeholder="请输入线下价格" type="number" clearable :style="{width: '100%'}">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="时间" prop="starttime">
+                            <el-date-picker
+                                    v-model="formData1.startandendtime"
+                                    value-format="yyyy-MM-dd"
+                                    type="daterange"
+                                    range-separator="至"
+                                    start-placeholder="开始日期"
+                                    end-placeholder="结束日期">
+                            </el-date-picker>
+                        </el-form-item>
+                        <el-form-item
+                                v-for="(item,index) in desc"
+                                :label="'标题' + (index+1)"
+                                :prop="'desc.' + index + '.value'">
+                            <el-input
+                                    type="text"
+                                    placeholder="请输入标题"
+                                    v-model="desc[index].title"
+                                    maxlength="10"
+                                    show-word-limit
+                            >
+                            </el-input>
+                            <div style="margin: 20px 0;"></div>
+                            <el-input
+                                    type="textarea"
+                                    placeholder="请输入内容"
+                                    v-model="desc[index].content"
+                                    maxlength="30"
+                                    show-word-limit
+                            >
+                            </el-input>
+                            <el-button @click.prevent="addDesc">添加</el-button>
+                            <el-button v-if="index!=0" @click.prevent="removeDesc">删除</el-button>
+                        </el-form-item>
                     </el-form>
                     <div slot="footer">
                         <el-button @click="close1">取消</el-button>
-                        <el-button type="primary" @click="handelConfirm1">确定</el-button>
+                        <el-button type="primary" @click="handelConfirm1">更新</el-button>
                     </div>
                 </el-dialog>
                 <el-dialog  :visible.sync="dialogVisible2" title="编辑店铺" width="40%">
                     <el-collapse v-model="activeNames">
-                        <div v-for="item in course_detail">
-                        <el-collapse-item :title="item.title" name="1">
-                            <div>{{item.content}}</div>
-                        </el-collapse-item>
+                        <div v-for="(item,index) in course_detail">
+                            <el-collapse-item :title="item.title" :name="index">
+                                <div>{{item.content}}</div>
+                            </el-collapse-item>
                         </div>
                     </el-collapse>
                 </el-dialog>
@@ -152,14 +222,21 @@
     export default {
         data() {
             return {
+                text:"",
+                textarea:"",
                 pageindex:1,
                 visible:false,
                 inputid:"",
                 total:0,
                 tableData: [],
-                formData: {},
-                formData1:{},
-                activeNames: ['1'],
+                formData: {
+                },
+                desc:[{
+                    title:"",
+                    content:"",
+                }],
+                formData1: {},
+                activeNames: [],
                 dialogVisible:false,
                 dialogVisible1:false,
                 dialogVisible2:false,
@@ -167,55 +244,28 @@
                 course_detail:null,
                 rules: {
                     coursename: [
-                        { required: true, message: '请输入店铺名称', trigger: 'blur' },
+                        { required: true, message: '请输入课程名称', trigger: 'blur' },
                         { min: 3, max: 15, message: '长度在 3-15 个字符内', trigger: 'blur' }
                     ],
                     onlineprice: [
-                        { type: 'string',required: true, message: '请输入店铺类别', trigger: 'blur' }
+                        { type: 'string',required: true, message: '请输入线上价格', trigger: 'blur' }
                     ],
                     offlineprice: [
-                        { type: 'string', required: true, message: '请输入联系电话', trigger: 'blur' }
-                    ]
-                },
-                rules1: {
-                    name: [
-                        { required: true, message: '请输入店铺名称', trigger: 'blur' },
-                        { min: 3, max: 15, message: '长度在 3-15 个字符内', trigger: 'blur' }
-                    ],
-                    class: [
-                        { type: 'string',required: true, message: '请输入店铺类别', trigger: 'blur' }
-                    ],
-                    phonenum: [
-                        { type: 'string', required: true, message: '请输入联系电话', trigger: 'blur' }
-                    ],
-                    location: [
-                        { type: 'string', required: true, message: '请输入坐标', trigger: 'blur' }
-                    ],
-                    shortloc:[
-                        { type: 'string', required: true, message: '请输入所处区域', trigger: 'blur' }
-                    ],
-                    address:[
-                        { type: 'string', required: true, message: '请输入详细地址', trigger: 'blur' }
-                    ],
-                    keyword:[
-                        { type: 'string', required: true, message: '请输入关键词信息', trigger: 'blur' }
-                    ],
-                    salenum:[
-                        { type: 'string', required: true, message: '请输入店铺销量', trigger: 'blur' }
-                    ],
-                    ratenum:[
-                        { type: 'string', required: true, message: '请输入店铺评分', trigger: 'blur' }
-                    ],
-                    startprice:[
-                        { type: 'string', required: true, message: '请输入起始价格', trigger: 'blur' }
-                    ],
-                    score:[
-                        { type: 'string', required: true, message: '请输入店铺评分', trigger: 'blur' }
+                        { type: 'string', required: true, message: '请输入线下价格', trigger: 'blur' }
                     ]
                 },
             }
         },
         methods:{
+            addDesc(item){
+                this.desc.push({
+                    title: "",
+                    content: ""
+                })
+            },
+            removeDesc(item){
+                this.desc.pop()
+            },
             courseDetail(item){
                 this.course_detail = item
                 this.dialogVisible2 = true
@@ -224,7 +274,7 @@
                 console.log(item)
                 var that =this
                 serviceMongo({
-                    url: "/businessinfo/delete?businessinfoid="+item,
+                    url: "/course/delete?courseid="+item,
                     method: "get",
                 }).then(function (res) {
                     if (res.data.Status=="success"){
@@ -238,12 +288,9 @@
             addCourse(){
                 this.dialogVisible = true
             },
-            editShop(item){
+            editCourse(item){
+                this.formData1._id = item._id
                 this.dialogVisible1 = true
-                this.formData1 = this.copy(item)
-            },
-            copy: function(obj) {
-                return JSON.parse(JSON.stringify(obj))
             },
             close(){
                 this.dialogVisible = false
@@ -252,6 +299,7 @@
                 this.dialogVisible1 = false
             },
             handelConfirm(){
+                var that = this
                 var businessid = localStorage.getItem("businessid")
                 serviceMongo({
                     url:"/course/insert",
@@ -261,19 +309,19 @@
                         coursename:this.formData.coursename,
                         onlineprice:this.formData.onlineprice,
                         offlineprice:this.formData.offlineprice,
-                        starttime:this.formData.starttime,
-                        endtime:this.formData.endtime,
-                        detail:this.formData.detail,
+                        starttime:this.formData.startandendtime[0],
+                        endtime:this.formData.startandendtime[1],
+                        detail:this.desc,
                     }
                 }).then(function (res) {
                     console.log(res.data)
-                    if (res.data.Status=="success"){
+                    if (res.data.status=="success"){
                         that.queryList(1,10)
-                        that.$message.success("更新成功")
+                        that.$message.success("插入成功")
                         that.dialogVisible =false
                         that.formData1 = {}
                     }else{
-                        that.$message.error("更新失败")
+                        that.$message.error("插入失败")
                         that.dialogVisible =false
                         that.fromData1 = {}
                     }
@@ -281,75 +329,31 @@
             },
             handelConfirm1(){
                 var that = this
-                if (this.formData1.name!=null&&
-                    this.formData1.class!=null&&
-                    this.formData1.phonenum!=null&&
-                    this.formData1.shortloc!=null&&
-                    this.formData1.address!=null&&
-                    this.formData1.keyword!=null&&
-                    this.formData1.salenum!=null&&
-                    this.formData1.ratenum!=null&&
-                    this.formData1.score!=null&&
-                    this.formData1.startprice!=null)
-                {
-                    if (this.formData1.name!=""&&
-                        this.formData1.class!=""&&
-                        this.formData1.phonenum!=""&&
-                        this.formData1.shortloc!=""&&
-                        this.formData1.address!=""&&
-                        this.formData1.keyword!=""&&
-                        this.formData1.salenum!=""&&
-                        this.formData1.ratenum!=""&&
-                        this.formData1.score!=""&&
-                        this.formData1.startprice!="")
-                    {
-                        if (typeof(this.formData1.location)=="string"){
-                            this.formData1.location = this.formData1.location.split("|")
-                            var la = Number(this.formData1.location[0])
-                            var lo = Number(this.formData1.location[1])
-                            this.formData1.location.coordinates = [la,lo]
-                        }
-                        console.log(this.formData1.location)
-                        serviceMongo({
-                            url:"/businessinfo/update",
-                            method:"post",
-                            data:{
-                                objid:this.formData1._id,
-                                name:this.formData1.name,
-                                class:this.formData1.class,
-                                phonenum:this.formData1.phonenum,
-                                location:{
-                                    type : "Point",
-                                    coordinates : this.formData1.location.coordinates
-                                },
-                                shortloc:this.formData1.shortloc,
-                                address:this.formData1.address,
-                                keyword:this.formData1.keyword,
-                                score:this.formData1.score,
-                                ratenum:this.formData1.ratenum,
-                                salenum:this.formData1.salenum,
-                                startprice:this.formData1.startprice
-                            }
-                        }).then(function (res) {
-                            console.log(res.data)
-                            if (res.data.Status=="success"){
-                                that.queryList(1,10)
-                                that.$message.success("更新成功")
-                                that.dialogVisible =false
-                                that.formData1 = {}
-                            }else{
-                                that.$message.error("更新失败")
-                                that.dialogVisible =false
-                                that.fromData1 = {}
-                            }
-                        })
-
-                    }else{
-                        that.$message.warning("有未填内容")
+                serviceMongo({
+                    url:"/course/update",
+                    method:"post",
+                    data:{
+                        courseid:this.formData1._id,
+                        coursename:this.formData1.coursename,
+                        onlineprice:this.formData1.onlineprice,
+                        offlineprice:this.formData1.offlineprice,
+                        starttime:this.formData1.startandendtime[0],
+                        endtime:this.formData1.startandendtime[1],
+                        detail:this.desc,
                     }
-                }else{
-                    that.$message.warning("有未填内容")
-                }
+                }).then(function (res) {
+                    console.log(res.data)
+                    if (res.data.Status=="success"){
+                        that.queryList(1,10)
+                        that.$message.success("更新成功")
+                        that.dialogVisible1 =false
+                        that.formData1 = {}
+                    }else{
+                        that.$message.error("更新失败")
+                        that.dialogVisible1 =false
+                        that.fromData1 = {}
+                    }
+                })
             },
             current(index){
                 this.queryList(index,10)
